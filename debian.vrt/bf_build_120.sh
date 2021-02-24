26f2f6569d4aac529df32798a79ccaf9da71c9ef
xcir <kokoniimasu@gmail.com>
2021-02-24 06:39:06 +0000

    mod

--- debian.vrt/bf_build_120.sh
+++ debian.vrt/bf_build_120.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 curl -s https://packagecloud.io/install/repositories/varnishcache/varnish65/script.deb.sh | sudo bash
-sudo apt-get install varnish=6.5.1~focal-1 varnish-dev=6.5.1~focal-1
+sudo apt-get install varnish varnish-dev
 
